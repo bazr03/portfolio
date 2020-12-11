d17a7e5fe6cbb3b470957ac2a05b1d9830218d9e
Iván Bazaldúa <icesarbr@gmail.com>
2020-12-10 18:15:49 -0600

    mobile nav in progress

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,26 @@ import "../sass/main.scss";
 
 
 /* Importar imagenes */
-import "../static/Logo_Iván_Bazaldúa.png"
+// import "../static/Logo_Iván_Bazaldúa.png"
+import "../static/Logo_ICBR1_v2.png"
 import "../static/heroBkgimg.jpg"
-import "../static/Ivan-web-small1.png"
+import "../static/Ivan-web-small1.png";
+import "../static/logos_html-5.png";
+import "../static/logos_css-3.png";
+import "../static/logos_javascript.png";
+import "../static/logos_webpack.png";
+import "../static/logos_sass.png";
+import "../static/fa-brands_git-alt.png";
+import "../static/grommet-icons_figma.png";
+
+import "../static/logos_php.png";
+import "../static/logos_laravel.png";
+import "../static/logos_python.png";
+import "../static/logos_django.png";
+import "../static/dashicons_database.png";
+import "../static/logos_linux-tux.png";
+import "../static/logos_angular-icon.png";
+
 
 /*--------------------------
 ------- SELECTORES ----------
@@ -21,7 +38,7 @@ observadores();
 
 function observadores() {
   const heroOptions = {
-    rootMargin: "-140px"
+    rootMargin: "-180px"
   };
 
   const heroObserver = new IntersectionObserver(function(
